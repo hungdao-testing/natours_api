15a8749488ec7c86bed675874bceab9aee7551b7
hungdao-testing <hung.dao@unifiedpost.com>
2022-02-17 19:29:50 +0700

    lecture 118: set log for dev and prod env

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,11 +1,51 @@
-import { NextFunction, Request, Response } from "express";
-import AppError from "../utils/appError";
+import { NextFunction, Request, Response } from 'express';
+import AppError from '../utils/appError';
 
-export default function errorController(err: AppError, req: Request, res: Response, next: NextFunction) {
-    err.statusCode = err.statusCode;
-    err.status = err.status || 'error';
+const sendErrorDev = (err: AppError, res: Response) => {
     res.status(err.statusCode!).json({
         status: err.status,
-        message: err.message
-    })
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
+};
+
+const sendErrorProd = (err: AppError, res: Response) => {
+    // Operational, trusted error: send message to client
+
+    if (err.isOperational) {
+        res.status(err.statusCode!).json({
+            status: err.status,
+
+            message: err.message
+        });
+
+
+    }
+    // Programming or other unknown error: dont leak detail
+    else {
+        console.error('ERROR: ', err);
+
+        res.status(500).json({
+            status: 'error',
+
+            message: 'Something went wrong!!!'
+        });
+    }
+};
+
+export default function errorController(
+    err: AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    err.statusCode = err.statusCode;
+    err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, res);
+    }
 }
