574af05b9167eaf34f2beadd953e57498675e376
hungdao-testing <hung.dao@unifiedpost.com>
2022-02-21 22:53:07 +0700

    lecture 127: password management

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
-
+import bcrypt from 'bcryptjs';
 
 export interface IUser extends mongoose.Document {
     name: string;
     email: string;
     photo?: string;
     password: string;
-    passwordConfirm: string;
+    passwordConfirm: string | undefined;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
@@ -25,13 +25,27 @@ const userSchema = new mongoose.Schema<IUser>({
         required: [true, 'Please provide password'],
         minlength: [8, 'The min length is at least 8 chars']
     },
-    passwordConfirm: { type: String, required: [true, 'Please provide password'] , validate: {
-        //THIS only works on save
-        validator: function(this: IUser ,el: string){
-            return el === this.password;
+    passwordConfirm: {
+        type: String, required: [true, 'Please provide password'], validate: {
+            //THIS only works on save
+            validator: function (this: IUser, el: string) {
+                return el === this.password;
+            },
+            message: "Password are not the same as"
         }
-    }}
+    }
 });
 
+userSchema.pre('save', async function (this: IUser, next) {
+    // Only run this function if password is actually modified
+    if (!this.isModified('password')) next();
+
+    // Hash password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm
+    this.passwordConfirm = undefined;
+
+})
 
 export const UserModel = mongoose.model<IUser>('User', userSchema);
