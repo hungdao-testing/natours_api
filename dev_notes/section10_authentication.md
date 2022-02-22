573987537fddd840c7523d28fec3f63aed203cc5
hungdao-testing <hung.dao@unifiedpost.com>
2022-02-22 17:13:50 +0700

    lecture 129: signup user

--- dev_notes/section10_authentication.md
+++ dev_notes/section10_authentication.md
@@ -1,3 +1,5 @@
 1. Lecture #127,   
 
     [`this` works only on 'create and save', therefore could not work on update/delete](https://mongoosejs.com/docs/validation.html#update-validators-and-this)
+
+2. Lecture #129, the length of JWT_SECRET should be at least 32 chars
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserModel } from '../models/userModel';
 import { catchAsync } from '../utils/catchAsync';
+import jwt from 'jsonwebtoken';
 
+export const signup = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const newUser = await UserModel.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm
+        });
 
-export const signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const newUser = await UserModel.create(req.body);
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    })
-})
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET!, {
+            expiresIn: process.env.JWT_EXPIRES_IN!
+        });
+
+        res.status(201).json({
+            status: 'success',
+            token,
+            data: {
+                user: newUser
+            }
+        });
+    }
+);
