7eee176bf5e47244c4f20bcbc1e1f183c6425c5d
hungdao-testing <hung.dao@unifiedpost.com>
2022-03-03 11:01:48 +0700

    lecture 151: modeling tour guides, embedding

--- src/models/tourModel.ts
+++ src/models/tourModel.ts
@@ -1,5 +1,6 @@
 import mongoose, { Aggregate } from 'mongoose'
 import slugify from 'slugify'
+import { IUser, UserModel } from './userModel'
 
 type Location = {
   type: string
@@ -28,7 +29,8 @@ export interface ITour extends mongoose.Document {
   startDates: Date[]
   secretTour: boolean
   startLocation: Location
-  locations: (Location & { day: number })[]
+  locations: (Location & { day: number })[],
+  guides: (IUser | null)[]
 }
 
 const tourSchema = new mongoose.Schema<ITour>(
@@ -116,6 +118,7 @@ const tourSchema = new mongoose.Schema<ITour>(
         day: Number,
       },
     ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -136,11 +139,13 @@ tourSchema.pre('save', function (next) {
   next()
 })
 
-// tourSchema.pre('save', function (next) {
-//     console.log("Will save document");
-//     next();
+tourSchema.pre('save', async function (next) {
 
-// })
+  const guidesPromises = this.guides.map(async id => await UserModel.findById(id).select("-passwordChangedAt"));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+
+})
 
 // tourSchema.post('save', function (doc, next) {
 //     console.log(doc);
