dec032a24ebc3eb299729e8b758c81effd6663a6
hungdao-testing <hung.dao@unifiedpost.com>
2022-04-07 16:47:04 +0700

    lecture 184. Building the Tour Page - Part 1

--- src/main/controllers/view.controller.ts
+++ src/main/controllers/view.controller.ts
@@ -24,11 +24,18 @@ export const getOverview = catchAsync(async (
   })
 })
 
-export const getTour = (
+export const getTour = catchAsync(async (
   req: ICustomRequestExpress,
   res: ICustomResponseExpress,
 ) => {
+  // 1) get the data, for the requested tour (including reviews and tour guide)
+  const tour = await TourModel.findOne({ slug: req.params.slug }).populate({ path: 'reviews', fields: 'review rating user' })
+
+  //2 ) Build template
+
+  // 3) render template
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour
   })
-}
+})
--- src/main/routes/view.routes.ts
+++ src/main/routes/view.routes.ts
@@ -4,6 +4,6 @@ import * as viewsController from '../controllers/view.controller'
 const router = express.Router()
 
 router.get('/', viewsController.getOverview)
-router.get('/tour', viewsController.getTour)
+router.get('/tour/:slug', viewsController.getTour)
 
 export default router
