574a70ea94163c41067dd6ea7f33d4f5bd808c02
hungdao-testing <hung.dao@unifiedpost.com>
2022-04-20 21:52:42 +0700

    logout

--- src/main/routes/user.routes.ts
+++ src/main/routes/user.routes.ts
@@ -8,6 +8,8 @@ router.post('/signup', authController.signup)
 
 router.post('/login', authController.login)
 
+router.get('/logout', authController.logout)
+
 router.post('/forgotPassword', authController.forgotPassword)
 
 router.patch('/resetPassword/:token', authController.resetPassword) // edit password => using PATCH or PUT, but edit just a portion of User data => PATCH
