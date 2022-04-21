d12651ba1f4ed6792275c71eade878bc09b41584
hungdao-testing <hung.dao@unifiedpost.com>
2022-04-21 11:05:07 +0700

    update package and tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,24 @@
 import { PlaywrightTestConfig } from '@playwright/test'
+import dotenv from 'dotenv'
 
+import { appConfig } from './config/env.config'
+
+if (process.env.NODE_ENV === 'development') {
+    dotenv.config({ path: appConfig.dev_env })
+} else if (process.env.NODE_ENV === 'local') {
+    dotenv.config({ path: appConfig.local_env })
+} else if (process.env.NODE_ENV === 'production') {
+    dotenv.config({ path: appConfig.prod_env })
+}
+
+const port = process.env.PORT;
 
 const config: PlaywrightTestConfig = {
     projects: [
         {
             name: 'api',
             use: {
-                baseURL: `http://localhost:3001`,
+                baseURL: `http://localhost:${port}`,
             },
             testMatch: /.*.spec.ts/,
             testDir: "./src/tests/"
