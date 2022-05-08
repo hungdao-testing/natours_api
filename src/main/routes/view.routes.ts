b94f499d17fc40b1969562da8c70e3eda701143f
LAPTOP-TQO2UI66\hung.dao_unifiedpost <hung.dao@unifiedpost.com>
2022-05-08 18:05:33 +0700

    lecture 215. Rendering a User's Booked Tours

--- src/main/routes/view.routes.ts
+++ src/main/routes/view.routes.ts
@@ -11,6 +11,8 @@ router.get('/', bookingController.createBookingCheckout, authController.isLogged
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour)
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm)
 router.get('/me', authController.protect, viewsController.getAccount)
+router.get('/my-tours', authController.protect, viewsController.getMyTours)
+
 router.post(
   '/submit-user-data',
   authController.protect,
