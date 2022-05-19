73f94717511a3668a96b96ae7ef4101c36f6b0b2
LAPTOP-TQO2UI66\hung.dao_unifiedpost <hung.dao@unifiedpost.com>
2022-05-19 22:02:20 +0700

    Heroku

--- build_script.ts
+++ build_script.ts
@@ -2,15 +2,12 @@ import {cp} from 'shelljs'
 
 const buildFolder = './dist/';
 
-// const files = new Set(['.env', 'LICENSE', 'README.md', 'package.json', 'package-lock.json']);
-const folders = new Set(['./data', './config', './src/public', './src/main/views']);
-
-// Copy Files
-// files.forEach((file) => {
-//   shell.cp('-R', file, buildFolder);
-// });
+const featureFolders = new Set(['./data', './config', './src/public', './src/main']);
 
 // Copy Folders
-folders.forEach((folder) => {
+featureFolders.forEach((folder) => {
   cp('-R', folder, buildFolder);
 });
+
+const viewsFolder = './src/main/views/'
+cp('-R', viewsFolder, './dist/src/main')
