ab97602a02286646fef24cbf710c300cbf9f274f
LAPTOP-TQO2UI66\hung.dao_unifiedpost <hung.dao@unifiedpost.com>
2022-05-19 17:40:33 +0700

    Heroku

---
+++ build_script.ts
@@ -0,0 +1,16 @@
+import {cp} from 'shelljs'
+
+const buildFolder = './dist/';
+
+// const files = new Set(['.env', 'LICENSE', 'README.md', 'package.json', 'package-lock.json']);
+const folders = new Set(['./data']);
+
+// Copy Files
+// files.forEach((file) => {
+//   shell.cp('-R', file, buildFolder);
+// });
+
+// Copy Folders
+folders.forEach((folder) => {
+  cp('-R', folder, buildFolder);
+});
