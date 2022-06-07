b206ecdfe0690eabd914dbddcd331491bf5d5020
LAPTOP-TQO2UI66\hung.dao_unifiedpost <hung.dao@unifiedpost.com>
2022-06-07 10:14:31 +0700

    implement stripe message

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -69,4 +69,4 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if(alert) showAlert('success', alertMessage, 20)
+if(alertMessage) showAlert('success', alertMessage, 20)
