635893ec9d78bcbddf8095c080d24d2e984d948c
hungdao-testing <daoquanghunggtvt@gmail.com>
2022-06-09 17:46:58 +0700

    setup basic playwright

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+import { environment } from '@config/env.config';
+
+
+
+const config: PlaywrightTestConfig = {
+    use: {
+        baseURL: `localhost:${environment.PORT}`
+    },
+    testDir: 'tests',
+    testIgnore: '**/test-assets',
+    testMatch: '.*(spec)\.ts',
+    workers: process.env.CI ? 2 : undefined,
+    projects: [
+        {
+            name: "rest_api",
+            testDir: 'specs/api',
+
+        },
+        {
+            name: 'ui-chromium',
+            use: { ...devices['Desktop Chrome'] },
+            testDir: 'specs/ui',
+        },
+        {
+            name: 'ui-firefox',
+            use: { ...devices['Desktop Firefox'] },
+            testDir: 'specs/ui',
+        },
+    ]
+}
+
+export default config;
